6e3213e177267ec03c8888c7636e175f07cab8ff
Léon Lenclos <leon.lenclos@gmail.com>
2025-12-07 21:50:52 +0100

    cards browser

--- assets/js/cards-browser.js
+++ assets/js/cards-browser.js
@@ -7,8 +7,20 @@ document.addEventListener("DOMContentLoaded", function() {
   document.querySelector('#cards-treatments').addEventListener('change',(e)=>{
     changeTreatments(e.target.value)
   })
+
+  document.querySelector('#filter-string').addEventListener('input',(e)=>{
+    if(e.target.value.trim().length) filterCards(e.target.value.trim());
+  });
+
+
+
 });
 
+function filterCards(str){
+    let selector = document.querySelector('#search-in').value;
+    let cards = document.querySelectorAll('.cards .card');
+    filterCards(cards, selector, str);
+}
 
 function changeTreatments(treatment){
   document.querySelectorAll('.cards .card').forEach((card)=>{
@@ -16,3 +28,19 @@ function changeTreatments(treatment){
     card.querySelector('.treatment').innerHTML = treatment;
   });
 }
+
+
+function filterCards(cards, selector, str){
+  const removeDiatrics = (str) => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  str = removeDiatrics(str.toLowerCase();
+  cards.forEach((card)=>{
+    let cardText = card.querySelector(selector).innerText;
+    cardText = removeDiatrics(cardText.toLowerCase();
+    if(cardText.indexOf(str)>=0){
+      card.style.display='';
+    }
+    else {
+      card.style.display='none';
+    }
+  });
+} 
