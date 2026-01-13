c81d355dfd16b5dd76e962e8cd524ff6b424815d
Léon Lenclos <leon.lenclos@gmail.com>
2026-01-13 16:02:26 +0100

    ('<_ '

--- assets/js/autoqrcode.js
+++ assets/js/autoqrcode.js
@@ -9,8 +9,11 @@ function setupQrCodes(){
         width: element.dataset.qrcodeWidth || 128,
         height: element.dataset.qrcodeHeight || 128,
         colorDark : element.dataset.qrcodeColorDark || "#000000",
-        colorLight : element.dataset.qrcodeColorLight || "#ffffff"
+        colorLight : element.dataset.qrcodeColorLight || "#ffffff",
+      	correctLevel : QRCode.CorrectLevel.M
       });
+      qrcode.clear(); // clear the code.
+      qrcode.makeCode(element.dataset.qrcodeUrl); // make another code.
       console.log("Generate a qrcode :", qrcode);
   });
 }
