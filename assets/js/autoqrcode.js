bd38ffed205375639b8321f1d742bc94da06a45f
Léon Lenclos <leon.lenclos@gmail.com>
2026-01-13 15:26:30 +0100

    bug qrcode

--- assets/js/autoqrcode.js
+++ assets/js/autoqrcode.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function setupQrCodes(){
   document.querySelectorAll('[data-qrcode-url]').forEach((element)=>{
-      console.log("Generate a qrcode :" qrcode)
       let qrcode = new QRCode(element, {
-      text: element.dataset.qrcodeUrl,
-      width: element.dataset.qrcodeWidth || 128,
-      height: element.dataset.qrcodeHeight || 128,
-      colorDark : element.dataset.qrcodeColorDark || "#000000",
-      colorLight : element.dataset.qrcodeColorLight || "#ffffff"
-    });
+        text: element.dataset.qrcodeUrl,
+        width: element.dataset.qrcodeWidth || 128,
+        height: element.dataset.qrcodeHeight || 128,
+        colorDark : element.dataset.qrcodeColorDark || "#000000",
+        colorLight : element.dataset.qrcodeColorLight || "#ffffff"
+      });
+      console.log("Generate a qrcode :", qrcode);
   });
 }
