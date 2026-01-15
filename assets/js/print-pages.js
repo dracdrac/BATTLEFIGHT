6e3ba46eaeadb724bc72d154c7aad48278059774
Léon Lenclos <leon.lenclos@gmail.com>
2026-01-15 14:56:37 +0100

    export png

--- assets/js/print-pages.js
+++ assets/js/print-pages.js
@@ -4,4 +4,27 @@ document.addEventListener("DOMContentLoaded", function() {
    console.log(e)
     print();
   });
+
+  document.querySelector('#export').addEventListener('click', (e)=>{
+   console.log(e)
+    let pages = document.querySelectorAll('.page').forEach((page, i)=>{
+      downloadHTMLAsImage(page, i);
+    });
+  });
 });
+
+
+function downloadCanvasAsImage(canvas, name){
+  var link = document.createElement('a');
+  link.download = name + 'png';
+  link.href = canvas.toDataURL();
+  link.click();
+}
+
+function downloadHTMLAsImage(element, i){
+html2canvas(element).then(canvas => {
+    let name = element.id ? element.id : 'sanstitre' + i;
+    downloadCanvasAsImage(canvas, element.id);
+});
+
+}
